eca8b86900ecf98ee9195ae8fcb61c672aa861ef
Alnur Alimanov <56869115+AAleka@users.noreply.github.com>
2023-12-07 14:50:23 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    a.setWindowIcon(QIcon(":/images/imgs/OnlyTasksIcon.png"));
+    //a.setWindowIcon(QIcon(":/images/imgs/OnlyTasksIcon.png"));
 
     /* ---------------------- Style ---------------------- */
     QFile styleSheetFile(":/styles/styles/Adaptic.qss");
